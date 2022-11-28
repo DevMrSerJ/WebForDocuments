163bffb96a378f8cb74b2a9b6f2b007d7cdacd0b
DevMrSerJ <58230079+DevMrSerJ@users.noreply.github.com>
2022-11-28 22:35:35 +0300

    Add pages and routing

---
+++ WebForDocuments/ClientApp/src/app/all-documents-page/all-documents-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-documents-page',
+  templateUrl: './all-documents-page.component.html',
+  styleUrls: ['./all-documents-page.component.scss']
+})
+export class AllDocumentsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- WebForDocuments/ClientApp/src/app/app.module.ts
+++ WebForDocuments/ClientApp/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AllDocumentsPageComponent } from './all-documents-page/all-documents-page.component';
+import { DocumentPageComponent } from './document-page/document-page.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AllDocumentsPageComponent },
+  { path: 'document', component: DocumentPageComponent },
+  { path: '**', component: ErrorPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllDocumentsPageComponent,
+    DocumentPageComponent,
+    ErrorPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
