43f4c34d190373afb18cb54c00da0e7819983bfc
DevMrSerJ <58230079+DevMrSerJ@users.noreply.github.com>
2022-11-29 22:55:45 +0300

    Update angular to 15 version

--- WebForDocuments/ClientApp/src/app/app.module.ts
+++ WebForDocuments/ClientApp/src/app/app.module.ts
@@ -1,30 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AllDocumentsPageComponent } from './all-documents-page/all-documents-page.component';
-import { DocumentPageComponent } from './document-page/document-page.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-const appRoutes: Routes = [
-  { path: '', component: AllDocumentsPageComponent },
-  { path: 'document', component: DocumentPageComponent },
-  { path: '**', component: ErrorPageComponent }
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AllDocumentsPageComponent,
-    DocumentPageComponent,
-    ErrorPageComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes),
-    NgbModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
