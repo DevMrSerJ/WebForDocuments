33592f77ae50bf3ac27d7111428d542ff28cde1f
DevMrSerJ <58230079+DevMrSerJ@users.noreply.github.com>
2022-11-29 23:38:19 +0300

    Add pages and routing

---
+++ WebForDocuments/ClientApp/src/app/all-documents-page/all-documents-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-documents-page',
+  templateUrl: './all-documents-page.component.html',
+  styleUrls: ['./all-documents-page.component.scss']
+})
+export class AllDocumentsPageComponent {
+
+}
--- WebForDocuments/ClientApp/src/app/app.module.ts
+++ WebForDocuments/ClientApp/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { AllDocumentsPageComponent } from './all-documents-page/all-documents-page.component';
+import { DocumentInfoPageComponent } from './document-info-page/document-info-page.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AllDocumentsPageComponent },
+  { path: 'documents', component: DocumentInfoPageComponent },
+  { path: '**', component: ErrorPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllDocumentsPageComponent,
+    DocumentInfoPageComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    NgbAlertModule
+    NgbAlertModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
